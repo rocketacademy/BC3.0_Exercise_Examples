5bc0d01fc982f5b88b098c1735d5e527a0c57a27
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-07-03 15:52:38 +0800

    adding in guess the word

--- m1/guess-the-word-bootcamp/src/App.js
+++ m1/guess-the-word-bootcamp/src/App.js
@@ -2,27 +2,44 @@ import React from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
 
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(require.context("./images", true, /\.(PNG|png)$/));
+
 class App extends React.Component {
   constructor(props) {
-    // Always call super with props in constructor to initialise parent class
     super(props);
     this.state = {
-      // currWord is the current secret word for this round. Update this with this.setState after each round.
       currWord: getRandomWord(),
-      // guessedLetters stores all letters a user has guessed so far
       guessedLetters: [],
-      // Insert num guesses left state here
       numberOfGuessesLeft: 10,
       guess: "",
       round: 0,
       wins: 0,
-      // Insert form input state here
+      roundFinished: false,
     };
   }
 
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyPressed);
+  }
+
+  keyPressed = (e) => {
+    if (this.state.roundFinished === false) {
+      if (e.key === "Enter" && this.state.numberOfGuessesLeft > 0) {
+        this.handleSubmit(e);
+      }
+    }
+  };
+
   generateWordDisplay = () => {
     const wordDisplay = [];
-    // for...of is a string and array iterator that does not use index
     for (let letter of this.state.currWord) {
       if (this.state.guessedLetters.includes(letter)) {
         wordDisplay.push(letter);
@@ -39,74 +56,94 @@ class App extends React.Component {
         .split("")
         .every((e) => this.state.guessedLetters.includes(e))
     ) {
-      alert("You win!!");
       this.setState({
-        round: this.state.round + 1,
         wins: this.state.wins + 1,
+        roundFinished: true,
+        round: this.state.round + 1,
       });
     }
   };
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    if (this.state.numberOfGuessesLeft < 1) {
-      alert("You have lost");
-      this.setState({
-        guessedLetters: this.state.guessedLetters.concat(
-          ...this.state.currWord
-        ),
-        round: this.state.round + 1,
-      });
-    } else if (this.state.guess.length > 0) {
-      await this.setState({
-        guessedLetters: this.state.guessedLetters.concat(this.state.guess[0]),
-        numberOfGuessesLeft: this.state.numberOfGuessesLeft - 1,
-        guess: "",
-      });
-      this.checkWin();
+  handleSubmit = (e) => {
+    if (this.state.guess.length > 0) {
+      this.setState(
+        {
+          guessedLetters: this.state.guessedLetters.concat(this.state.guess[0]),
+          numberOfGuessesLeft: this.state.numberOfGuessesLeft - 1,
+          guess: "",
+        },
+        () => {
+          this.checkWin();
+          if (this.state.numberOfGuessesLeft === 0) {
+            this.setState({
+              round: this.state.round + 1,
+            });
+          }
+        }
+      );
     } else {
       alert("please insert some text into the input");
     }
+  };
+
+  reset = () => {
+    this.setState({
+      currWord: getRandomWord(),
+      guessedLetters: [],
+      numberOfGuessesLeft: 10,
+      guess: "",
+      roundFinished: false,
+    });
   };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Guess The Word 🚀</h1>
+          <div className="Container">
+            <div className="Column">
+              <h3>Word Display</h3>
+              {this.generateWordDisplay()}
+              <div className="Image Container">
+                Guesses Left: {this.state.numberOfGuessesLeft}
+                <img
+                  src={images[`hm${10 - this.state.numberOfGuessesLeft}.PNG`]}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="Column">
+              <h3>Guessed Letters</h3>
+              {this.state.guessedLetters.length > 0
+                ? this.state.guessedLetters.toString()
+                : "-"}
+              <h3>Input</h3>
+              <input
+                type="text"
+                value={this.state.guess}
+                max="1"
+                onChange={(e) =>
+                  this.setState({ guess: e.target.value.toLowerCase() })
+                }
+              />
+
+              {this.state.numberOfGuessesLeft === 0 ? (
+                <div>
+                  <h3>You have no lives left. Please restart</h3>
+                  <h3>Correct word: {this.state.currWord}</h3>
+                </div>
+              ) : this.state.roundFinished ? (
+                <h3>You have won. Please restart</h3>
+              ) : (
+                <button onClick={(e) => this.handleSubmit(e)}>submit</button>
+              )}
+              <button onClick={() => this.reset()}>Reset</button>
+            </div>
+          </div>
+
           <h3>
             Won: {this.state.wins} -- Round: {this.state.round}
           </h3>
-          <h3>Word Display</h3>
-          {this.generateWordDisplay()}
-          <h3>Guesses left: {this.state.numberOfGuessesLeft}</h3>
-          <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
-            ? this.state.guessedLetters.toString()
-            : "-"}
-          <h3>Input</h3>
-          <form>
-            <input
-              type="text"
-              value={this.state.guess}
-              max="1"
-              onChange={(e) => this.setState({ guess: e.target.value })}
-            />{" "}
-            <button onClick={(e) => this.handleSubmit(e)}>submit</button>
-          </form>
-          <button
-            onClick={() =>
-              this.setState({
-                currWord: getRandomWord(),
-                // guessedLetters stores all letters a user has guessed so far
-                guessedLetters: [],
-                // Insert num guesses left state here
-                numberOfGuessesLeft: 10,
-                guess: "",
-              })
-            }
-          >
-            Reset
-          </button>
         </header>
       </div>
     );
